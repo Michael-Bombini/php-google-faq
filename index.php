6706258a961a1143610b2d792f486998c64d30cb
Michael Bombini <bombini.michael@gmail.com>
2022-07-11 15:37:52 +0200

    List added

--- index.php
+++ index.php
@@ -32,34 +32,35 @@ $db = [
             "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:",
         ],
         "subtitle" => "Stabilire il paese associato al tuo account",
-        "subparagraph" => 
+        "subparagraph" =>
         [
             "Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.",
             "I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.",
             "Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. <a href='#'>Contattaci</a> se ritieni che il paese associato al tuo account sia sbagliato.",
         ],
-        "list" => 
+        "list" =>
         [
             "La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:",
             "La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.",
-        ],
-        "sublist" => 
+            "sublist" =>
         [
             "Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.",
             "Google LLC, con sede negli Stati Uniti, per il resto del mondo.",
         ]
+        ],
+        
 
     ],
 
     [
-    "title" => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
-    "paragraph" =>
-    [
-        "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni.",
+        "title" => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
+        "paragraph" =>
+        [
+            "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni.",
+        ],
+
     ],
-    
-    ],
-    
+
     [
 
         "title" => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
@@ -102,26 +103,22 @@ $db = [
             foreach ($faq["paragraph"] as $text) {
                 echo "<p>" . $text . "</p>";
             }
-            if (key_exists("list" , $faq)){
+            if (key_exists("list", $faq)) {
                 echo "<ol>";
-                foreach($faq["list"] as $item){
+                foreach ($faq["list"] as $item) {
                     echo "<li>" . $item . "</li>";
-                    if(key_exists("sublist", $faq)){
-                        foreach($faq["sublist"] as $subitem){
+                    if (key_exists("sublist", $faq["list"])) {
+                        foreach ($faq["list"]["sublist"] as $subitem) {
                             echo "<li>" . $subitem . "</li>";
-    
-                            }
-                      
-
+                        }
                     }
                 }
                 echo "</ol>";
             }
-            if (key_exists("subtitle", $faq)){
+            if (key_exists("subtitle", $faq)) {
                 echo "<h5>"  . $faq["subtitle"] . "</h5>";
                 foreach ($faq["subparagraph"] as $subtext)
-                echo "<p>" . $subtext . "</p>"; 
-
+                    echo "<p>" . $subtext . "</p>";
             }
 
             echo "</div>";
